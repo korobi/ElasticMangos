4870627addb17f06435632cf74be7ee1085ab2a1
lol768 <lol768@lol768.com>
2015-08-09 20:36:32 +0100

    Don't import blacklisted channels

---
+++ src/main/java/io/korobi/mongotoelastic/exception/ImportException.java
@@ -0,0 +1,8 @@
+package io.korobi.mongotoelastic.exception;
+
+public class ImportException extends RuntimeException {
+
+    public ImportException(String message) {
+        super(message);
+    }
+}
--- src/main/java/io/korobi/mongotoelastic/processor/MongoToElasticProcessor.java
+++ src/main/java/io/korobi/mongotoelastic/processor/MongoToElasticProcessor.java
@@ -1,6 +1,8 @@
 package io.korobi.mongotoelastic.processor;
 
+import io.korobi.mongotoelastic.exception.ImportException;
 import io.korobi.mongotoelastic.logging.InjectLogger;
+import io.korobi.mongotoelastic.mongo.IChannelBlacklist;
 import org.apache.logging.log4j.Logger;
 import org.bson.Document;
 import org.elasticsearch.action.bulk.BulkRequestBuilder;
@@ -19,10 +21,12 @@ public class MongoToElasticProcessor implements IDocumentProcessor {
     @InjectLogger
     private Logger logger;
     private Client esClient;
+    private IChannelBlacklist blacklist;
 
     @Inject
-    public MongoToElasticProcessor(Client esClient) {
+    public MongoToElasticProcessor(Client esClient, IChannelBlacklist blacklist) {
         this.esClient = esClient;
+        this.blacklist = blacklist;
     }
 
     @Override
@@ -30,35 +34,40 @@ public class MongoToElasticProcessor implements IDocumentProcessor {
         BulkRequestBuilder bulkRequest = this.esClient.prepareBulk();
 
         for (Document doc : documents) {
+            if (this.blacklist.isBlacklisted(doc.getString("network"), doc.getString("channel"))) {
+                continue;
+            }
+            
             try {
                 Date date = (Date) doc.get("date");
                 XContentBuilder docBuilder =
                     XContentFactory.jsonBuilder()
                         .startObject()
-                            .field("actor_hostname", (String) doc.get("actor_hostname"))
-                            .field("actor_name", (String) doc.get("actor_name"))
-                            .field("actor_prefix", (String) doc.get("actor_prefix"))
-                            .field("channel", (String) doc.get("channel"))
-                            // .field("channel_mode", (String) doc.get("channel_mode")) // TODO: Check w/ kashike
-                            .field("message", (String) doc.get("message"))
-                            .field("network", (String) doc.get("network"))
-                            .field("recipient_hostname", (String) doc.get("recipient_hostname"))
-                            .field("recipient_name", (String) doc.get("recipient_name"))
-                            .field("recipient_prefix", (String) doc.get("recipient_prefix"))
-                            .field("type", (String) doc.get("type"))
+                            .field("actor_hostname", doc.getString("actor_hostname"))
+                            .field("actor_name", doc.getString("actor_name"))
+                            .field("actor_prefix", doc.getString("actor_prefix"))
+                            .field("channel", doc.getString("channel"))
+                            // .field("channel_mode", doc.getString("channel_mode")) // TODO: Check w/ kashike
+                            .field("message", doc.getString("message"))
+                            .field("network", doc.getString("network"))
+                            .field("recipient_hostname", doc.getString("recipient_hostname"))
+                            .field("recipient_name", doc.getString("recipient_name"))
+                            .field("recipient_prefix", doc.getString("recipient_prefix"))
+                            .field("type", doc.getString("type"))
                             .field("mongoId", doc.get("_id").toString())
                             .field("date", date.getTime())
                         .endObject();
                 bulkRequest.add(this.esClient.prepareIndex("chats", "chat").setSource(docBuilder));
-                logger.info(docBuilder.string());
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-
         BulkResponse bulkResponse = bulkRequest.execute().actionGet();
         if (bulkResponse.hasFailures()) {
             logger.error("Bulk response failure! (this is bad) " + bulkResponse.getItems()[0].getFailureMessage());
+            throw new ImportException(bulkResponse.getItems()[0].getFailureMessage());
+        } else {
+            logger.info(String.format("%d docs were imported.", documents.size()));
         }
     }
 }
