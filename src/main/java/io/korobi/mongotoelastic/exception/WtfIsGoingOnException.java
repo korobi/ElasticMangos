3304c8cb49962cb654e0d348d210dedd9950606e
lol768 <lol768@lol768.com>
2015-09-01 00:58:19 +0100

    Add missed file

---
+++ src/main/java/io/korobi/mongotoelastic/exception/WtfIsGoingOnException.java
@@ -0,0 +1,8 @@
+package io.korobi.mongotoelastic.exception;
+
+public class WtfIsGoingOnException extends RuntimeException {
+
+    public WtfIsGoingOnException(String message) {
+        super(message);
+    }
+}
